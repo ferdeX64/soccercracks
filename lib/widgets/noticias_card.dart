f5c387a30fa996b19cdfdad38cbac16b69a3caf8
Erick Fernández <eafernandez3@espe.edu.ec>
2021-12-21 19:13:45 -0500

    Card

--- lib/widgets/noticias_card.dart
+++ lib/widgets/noticias_card.dart
@@ -7,15 +7,12 @@ class NoticeCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final url=model.imagen!.first;
-
     return Card(
         child:ListTile(
           leading:  Image.network(url,width: 64),
           title: Text(model.titulo??""),
           subtitle: Text(model.seccion?? ""),
-          ),
-           
-          
+          ), 
     );
   }
 }
